3eada67e11c99c12cc4d11fd525768ef294dab5f
Jake <jake@jake.local>
2018-10-17 20:09:36 -0700

    its the 80 comid

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,6 +1,6 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  root :to => 'movies#index'
   match '/movies/:id/similar(.:format)', to: 'movies#similar', as: 'movie_similar', via: :get
 end
